7d8b6e9a6956f1c681ac6120590ebea353fdc6a6
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2017-11-28 06:48:34 +0100

    feat(tests): unit and compose.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const dirtyChai = require('dirty-chai');
 const chaiAsPromised = require('chai-as-promised');
 const sinonChai = require('sinon-chai');
 
-const kmonads = require('../lib/kmonads');
+const kmonads = require('../src/kmonads');
 
 chai.use(dirtyChai);
 chai.use(chaiAsPromised);
@@ -22,12 +22,25 @@ describe('kmonads', () => {
 
 
   describe('#unit', () => {
-    it('expect to output an Array, for anything passed to.', () => {
-      expect(typeof kmonads.unit(undefined)).to.be.equal(typeof []);
-      expect(typeof kmonads.unit([])).to.be.equal(typeof []);
-      expect(typeof kmonads.unit('')).to.be.equal(typeof []);
-      expect(typeof kmonads.unit(6)).to.be.equal(typeof []);
-      expect(typeof kmonads.unit(null)).to.be.equal(typeof []);
+    it('expect to output an triplet, for anything passed to.', () => {
+      expect(kmonads.unit(undefined)).to.be.deep.equal([[undefined], ['Unit'], []]);
+      expect(kmonads.unit([])).to.be.deep.equal([[[]], ['Unit'], []]);
+      expect(kmonads.unit('')).to.be.deep.equal([[''], ['Unit'], []]);
+      expect(kmonads.unit(6)).to.be.deep.equal([[6], ['Unit'], []]);
+      expect(kmonads.unit(null)).to.be.deep.equal([[null], ['Unit'], []]);
+    });
+  });
+
+  describe('#compose', () => {
+    it('expect to take two function as parameter and "compose" them.', () => {
+      const f = sandbox.stub().callsFake(x => [[5 + x], ['F'], ['F error']]);
+      const g = sandbox.stub().callsFake(x => [[7 + x], ['G'], ['G error']]);
+      const out = kmonads.compose(f, g)([[1], ['start'], ['init error']]);
+      expect(out).to.deep.equal([
+        [1, 8, 13],
+        ['start', 'G', 'F'],
+        ['init error', 'G error', 'F error'],
+      ]);
     });
   });
 });
